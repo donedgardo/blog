6c48175e69158b1e104d16d553304f5e415b3a32
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2021-12-12 21:38:56 -0400

    update og title

--- src/components/seo.js
+++ src/components/seo.js
@@ -42,7 +42,7 @@ function SEO({ description, lang, meta, keywords, title }) {
         },
         {
           property: `og:title`,
-          content: site.siteMetadata.title,
+          content: `${title} | ${site.siteMetadata.title}`,
         },
         {
           property: `og:description`,

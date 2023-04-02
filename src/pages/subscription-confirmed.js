49568cfab94406751d443db21ed1dd897612f39b
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-04-02 04:10:38 -0400

    Add subscription confirmed page

---
+++ src/pages/subscription-confirmed.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+import BlogLayout from "../components/blog_layout"
+
+export default () => {
+  return (
+    <BlogLayout
+      location={"/subscription-confirmed"}
+      title={"Subscription Confirmed!"}
+    >
+      <p>
+        Thank you for confirming your email address! I'm excited to have you on
+        board and can't wait to share valuable insights with you.
+      </p>
+
+      <p>
+        If you have any questions or feedback, please don't hesitate to reach
+        out to us at me@edgardocarreras.com.
+      </p>
+      <p>
+        Thanks again for joining our community, and we look forward to staying
+        in touch!
+      </p>
+
+      <p>Best regards,</p>
+
+      <p>Edgardo Carreras</p>
+    </BlogLayout>
+  )
+}

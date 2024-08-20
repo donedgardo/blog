2a26f2288a0267aa4473ebca8e97ddd852cd8187
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2024-08-19 23:25:23 -0400

    Reset rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -177,71 +177,71 @@ module.exports = {
         enable_on_dev_env: false,
       },
     },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  title: node.frontmatter.title,
-                  description: node.frontmatter.description,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + "/daily/" + node.slug,
-                  guid: site.siteMetadata.siteUrl + "/daily/" + node.slug,
-                  custom_elements: [
-                    {
-                      "content:encoded": node.html.replace(
-                        /(?<=\"|\s)\/static\//g,
-                        `${site.siteMetadata.siteUrl}\/static\/`
-                      ),
-                    },
-                  ],
-                })
-              })
-            },
-            query: `
-              {
-                allMdx(
-                  filter: { frontmatter: { category: { eq: "daily" } } }
-                ) {
-                  nodes {
-                    slug 
-                    html
-                    frontmatter {
-                      title
-                      description
-                      date(formatString: "MMMM DD, YYYY")
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "Your Site's RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/daily/",
-            // optional configuration to specify external rss feed, such as feedburner
-            link: "https://feeds.feedburner.com/gatsby/blog",
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-feed`,
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             title
+    //             description
+    //             siteUrl
+    //             site_url: siteUrl
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     feeds: [
+    //       {
+    //         serialize: ({ query: { site, allMdx } }) => {
+    //           return allMdx.nodes.map(node => {
+    //             return Object.assign({}, node.frontmatter, {
+    //               title: node.frontmatter.title,
+    //               description: node.frontmatter.description,
+    //               date: node.frontmatter.date,
+    //               url: site.siteMetadata.siteUrl + "/daily/" + node.slug,
+    //               guid: site.siteMetadata.siteUrl + "/daily/" + node.slug,
+    //               custom_elements: [
+    //                 {
+    //                   "content:encoded": node.html.replace(
+    //                     /(?<=\"|\s)\/static\//g,
+    //                     `${site.siteMetadata.siteUrl}\/static\/`
+    //                   ),
+    //                 },
+    //               ],
+    //             })
+    //           })
+    //         },
+    //         query: `
+    //           {
+    //             allMdx(
+    //               filter: { frontmatter: { category: { eq: "daily" } } }
+    //             ) {
+    //               nodes {
+    //                 slug
+    //                 html
+    //                 frontmatter {
+    //                   title
+    //                   description
+    //                   date(formatString: "MMMM DD, YYYY")
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         `,
+    //         output: "/rss.xml",
+    //         title: "Your Site's RSS Feed",
+    //         // optional configuration to insert feed reference in pages:
+    //         // if `string` is used, it will be used to create RegExp and then test if pathname of
+    //         // current page satisfied this regular expression;
+    //         // if not provided or `undefined`, all pages will have feed reference inserted
+    //         match: "^/daily/",
+    //         // optional configuration to specify external rss feed, such as feedburner
+    //         link: "https://feeds.feedburner.com/gatsby/blog",
+    //       },
+    //     ],
+    //   },
+    // },
   ],
 }

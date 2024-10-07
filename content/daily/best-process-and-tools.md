e8ecd8dcd79e1fedc09c4ee2b095b2ae155ca2c2
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2024-10-07 02:30:18 -0400

    Add post and waitlist

---
+++ content/daily/best-process-and-tools.md
@@ -0,0 +1,45 @@
+---
+date: 2024-10-07T05:22:55.506Z
+title: "Best process and tools for software development?"
+category: daily
+---
+What's the best process and tools for software development?
+
+The ones your team chooses.
+
+As leaders, it is tempting to dictate the tools and processes you believe are best for the team.
+
+After all, consistency and control seem like pathways to efficiency.
+
+But here's a thought—what if giving your team these decisions could boost performance?
+
+Research and experience show that the best teams are trusted and autonomous.
+
+Let your teams act, make decisions, and then reflect.
+
+Action comes before reflection.
+
+This cycle of doing and evaluating builds both skills and team spirit.
+
+Teams that choose their tools and processes are more engaged.
+
+All while delivering better results.
+
+They become stakeholders in their destiny, and accountability emerges as a consequence.
+
+Empower your team today, and unlock their full potential.
+
+Yours,
+
+Ed
+
+---
+
+Do you want your team to learn software development practices that boost performance?
+
+I'm creating a whole new service called Learning Hours. A weekly hour-long session for your software team. 
+They will learn and practice TDD (test-driven development), refactoring, continuous delivery and more.
+
+Enable your team to deliver higher-quality software faster.
+
+Click here to join the waitlist: https://edgardocarreras.com/learning-hours-waitlist
---
+++ src/pages/learning-hours-waitlist.js
@@ -0,0 +1,94 @@
+import React from "react"
+import Seo from "../components/seo"
+import { Header } from "../components/header"
+
+function WaitListConfirmedHero() {
+  return (
+    <section id="hero" className="no-border-radius">
+      <div className="container">
+        <div className="row flex-md-nowrap">
+          <div className="col-1 col-12">
+            <h5 className="roboto-bold text-uppercase primary-color">
+              Learning Hour Waitlist Confirmed
+            </h5>
+            <h1 className="roboto-bold mb-1 mb-md-4 text-capitalize">
+              Your Spot is Reserved!
+            </h1>
+            <h4
+              className="roboto-light mb-1 mb-md-4"
+              style={{ maxWidth: "46.25rem" }}
+            >
+              Thank you for joining the Learning Hours waitlist!
+            </h4>
+            <h4
+              className="roboto-light mb-1 mb-md-4"
+              style={{ maxWidth: "46.25rem" }}
+            >
+              We're thrilled to have you onboard and can't wait to equip your
+              team with the skills needed to deliver higher-quality software at
+              speed. Your commitment to growth is a significant step toward
+              transforming your team's capabilities.
+            </h4>
+            <h4 className="roboto-bold mb-1 mb-md-4 text-capitalize">
+              What's Next?
+            </h4>
+            <h4 className="roboto-light" style={{ maxWidth: "46.25rem" }}>
+              You'll receive an email from me as soon as the sessions are open
+              for enrollment. Keep an eye on your inbox for further details and
+              start dates.
+            </h4>
+            <hr />
+            <h5 className="roboto-light" style={{ maxWidth: "46.25rem" }}>
+              If you have any questions or feedback, please don't hesitate to
+              reach out at me@edgardocarreras.com
+            </h5>
+            <p>Yours,</p>
+            <p>Ed</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const IndexPage = () => (
+  <>
+    <div className="site-content">
+      <Header />
+      <main>
+        <WaitListConfirmedHero />
+      </main>
+      <footer className="p-4 light-dark-bg">
+        <div className="bottom">
+          <p className="text-center text-white mb-1">
+            © {new Date().getFullYear()} Copyright. Edgardo Carreras.
+          </p>
+        </div>
+      </footer>
+    </div>
+  </>
+)
+
+export const Head = () => {
+  return (
+    <>
+      <Seo title={"Edgardo Carreras | SaaS Agile Technical Coach"} />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:wght@500&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="stylesheet" href="/styles/bootstrap.min.css" />
+      <link rel="stylesheet" href="/styles/bootstrap-grid.min.css" />
+      <link rel="stylesheet" href="/styles/styles.css" />
+      <style>
+        {` 
+          body {
+            background-color: #121212;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default IndexPage

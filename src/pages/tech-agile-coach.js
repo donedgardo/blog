0100fdae8f8fde68a0421c67d161240ea6c6a9f1
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2025-03-08 17:46:38 -0400

    Change seo on tech coach page

--- src/pages/tech-agile-coach.js
+++ src/pages/tech-agile-coach.js
@@ -1086,7 +1086,7 @@ const IndexPage = () => (
 export const Head = () => {
   return (
     <>
-      <Seo title={"Edgardo Carreras | SaaS Agile Technical Coach"} />
+      <Seo title={"Edgardo Carreras | Agile Technical Coach"} />
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:wght@500&display=swap"
         rel="stylesheet"
